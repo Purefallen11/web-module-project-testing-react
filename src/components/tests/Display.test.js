f6735ca012e122e5be8e8d1413864a01f4a190a3
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-10-29 00:58:35 -0500

    i messed up, i did npm i jest and i think it messed up my project

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,20 +1,45 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import Display from './../Display';
+import userEvent from '@testing-library/user-event'
+
+import mockFetchShow from '../../api/fetchShow'
+jest.mock('../../api/fetchShow')
+
+const testData = {
+	name: test,
+	summary: 'test test test',
+	seasons: [
+		{
+			id: 0,
+			name: 'seasons 1',
+			episode: []
+		},
+		{
+			id: 1,
+			name: 'season 2',
+			episode:[]
+		},
+		{
+			id: 2,
+			name: 'season 3',
+			episode: []
+		}
+	]
+}
 
 
+test("render without errors", () => {
+	render(<Display />)
+})
 
-
-
-
-
-
-
-
-
+test('')
 
 
 
 
 ///Tasks:
-//1. Add in nessisary imports and values to establish the testing suite.
+//1. Add in necessary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
 //3. Rebuild or copy a show test data element as used in the previous set of tests.
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
